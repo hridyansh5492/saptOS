d56ee25c11df900e845934e5cbc3c067a67e8416
hridyansh5492 <hridyanshsati4321@gmail.com>
2026-05-03 14:51:24 +0530

    new line functionality add

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,38 +1,24 @@
-void kernel_main() {
+int cursor = 0;
+
+void print(char *str) {
     char *video_memory = (char*) 0xb8000;
 
-    video_memory[0] = 'H';
-    video_memory[1] = 0x04;
+    for (int i = 0; str[i] != '\0'; i++) {
 
-    video_memory[2] = 'e';
-    video_memory[3] = 0x07;
+        if (str[i] == '\n') {
+            // move cursor to next line
+            cursor = (cursor / 160 + 1) * 160;
+        } else {
+            video_memory[cursor++] = str[i];
+            video_memory[cursor++] = 0x07;
+        }
+    }
+}
 
-    video_memory[4] = 'l';
-    video_memory[5] = 0x07;
-
-    video_memory[6] = 'l';
-    video_memory[7] = 0x07;
-
-    video_memory[8] = 'o';
-    video_memory[9] = 0x07;
-
-    video_memory[10] = ' ';
-    video_memory[11] = 0x07;
-
-    video_memory[12] = 'W';
-    video_memory[13] = 0x07;
-
-    video_memory[14] = 'o';
-    video_memory[15] = 0x07;
-
-    video_memory[16] = 'r';
-    video_memory[17] = 0x07;
-
-    video_memory[18] = 'l';
-    video_memory[19] = 0x07;
-
-    video_memory[20] = 'd';
-    video_memory[21] = 0x07;
+void kernel_main() {
+    print("Hello World\n");
+    print("This is my OS\n");
+    print("> ");
 
     while (1);
 }
