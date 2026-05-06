1dd584d92b6f8a97f5714a1dcc1160ad3bd75526
hridyansh5492 <hridyanshsati4321@gmail.com>
2026-05-06 19:01:23 +0530

    Add clear screen functionality and update kernel output

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,5 +1,16 @@
 int cursor = 0;
 
+void clear_screen() {
+    char *video_memory = (char*) 0xb8000;
+
+    for (int i = 0; i < 80 * 25 * 2; i += 2) {
+        video_memory[i] = ' ';      // empty character
+        video_memory[i + 1] = 0x07; // color
+    }
+
+    cursor = 0; // reset cursor to top
+}
+
 void print(char *str) {
     char *video_memory = (char*) 0xb8000;
 
@@ -17,7 +28,14 @@ void print(char *str) {
 
 void kernel_main() {
     print("Welcome to Sapt OS\n");
-    print("Simple Bootable Operating System\n\n");
+    print("Before clear...\n");
+
+    // delay loop (just for demo)
+    for (int i = 0; i < 100000000; i++);
+
+    clear_screen();
+
+    print("Screen Cleared!\n");
     print("> ");
 
     while (1);
